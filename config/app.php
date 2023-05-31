f1e33bc5cfacb1a59309e0d75ae0ad680c6d1033
Derek Bourgeois <derek@devoption.io>
2023-05-30 22:23:34 -0400

    #56 - install laravel telescope

--- config/app.php
+++ config/app.php
@@ -169,6 +169,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\FortifyServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\TelescopeServiceProvider::class,
     ])->toArray(),
 
     /*
